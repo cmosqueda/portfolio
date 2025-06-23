b17e8249d94519188975da8f0951ac57eb1ded5d
tyne <mosqueda.christinereisa04@gmail.com>
2025-06-23 17:05:59 +0800

    improved banner section

--- src/sections/Banner.tsx
+++ src/sections/Banner.tsx
@@ -9,7 +9,9 @@ const skillset = [
 ];
 
 export default function Banner() {
-  const [htmlCode, setHtmlCode] = useState(`
+  const [htmlCode, setHtmlCode] = useState(
+    `<!-- A simple HTML playground that you can try :D - Tine -->
+
 <h1 style="font-size:2.5rem; font-weight:800;">
   Hello, I'm Tine!
 </h1>
@@ -17,10 +19,11 @@ export default function Banner() {
   An aspiring
   <i><strong>Systems Analyst</strong></i>
 </p>
-`);
+`
+  );
 
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center p-6 my-20 text-white space-y-8">
+    <section className="min-h-screen flex flex-col items-center justify-center p-6 my-20 sm:my-5 text-white space-y-8">
       {/* Dual display container */}
       <div className="flex flex-col sm:flex-row gap-8 w-full max-w-6xl">
         {/* Input box */}
@@ -32,9 +35,9 @@ export default function Banner() {
               <span className="w-3 h-3 rounded-full bg-yellow-500"></span>
               <span className="w-3 h-3 rounded-full bg-green-500"></span>
             </div>
-            <div className="mt-6 text-green-400 text-xs mb-2">// you may edit the HTML below :D - Tine</div>
+
             <textarea
-              className="w-full h-48 bg-transparent text-white outline-none resize-none"
+              className="w-full h-50 mt-3 bg-transparent text-white outline-none resize-none"
               value={htmlCode}
               onChange={(e) => setHtmlCode(e.target.value)}
             />
@@ -45,7 +48,7 @@ export default function Banner() {
         <div className="flex flex-col w-full sm:w-1/2 space-y-2">
           <p className="text-sm font-semibold text-lvlnine">Output</p>
           <div
-            className="bg-white text-black rounded-lg p-5 shadow-md min-h-[12rem]"
+            className="bg-white text-black rounded-lg p-5 shadow-md h-full max-h-64 overflow-auto"
             dangerouslySetInnerHTML={{ __html: htmlCode }}
           />
         </div>
